f6055a9068134dce135867a902a7d796109523b9
t.lerias <t.lerias@tadaweb.com>
2017-09-01 12:58:44 +0200

    feature( header )

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <figure className="App-logo">
+            <span className="App-logo-text">TL</span>
+          </figure>
+          <figure className="App-header-text">
+            <h2 className="App-header-subtext">Tara Lerias</h2>
+            <h2 className="App-header-subtext">Front End Engineer</h2>
+          </figure>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
